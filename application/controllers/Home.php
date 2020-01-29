5019a43bc9886e225ec12ee50b2371e0b76b09a9
Naandalist <listiananda.apriliawan@gmail.com>
2020-01-29 13:41:40 +0700

    test using view

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,7 +3,7 @@
     class Home extends CI_Controller {
         public function index()
         {
-            echo "Home /index";
+            $this->load->view('home/index');
         }
     }
 ?>
