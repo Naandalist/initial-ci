b5abeeb71b36890c6cc8641cb305bd2a901fb1fa
Naandalist <listiananda.apriliawan@gmail.com>
2020-01-29 13:47:45 +0700

    separate header and footer in another view

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,12 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home / index</title>
-</head>
-<body>
-    <h1>Home /index </h1>
-</body>
-</html>
+
+    <h1>Hello, world! from bootstrap</h1>
